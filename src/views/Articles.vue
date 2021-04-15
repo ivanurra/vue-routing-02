03ec6c4374287123afb03d93b60cf62e1b208967
ivanurra <ivan.urra.wd@gmail.com>
2021-04-15 11:16:44 +0200

    params routes

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,54 +1,13 @@
 <template>
-    <div class="articles">
-        <Title text="Articles" />
-        <hr/>
-        <br/>
-        <!-- <button class="btn" @click="fetchApi">Fetch API</button> -->
-        <div class="titles" v-for="item in arrayBlog" :key="item.id">
-            {{item.id}} - {{ item.title }}
-        </div>
-    </div>
+<Title text="Params Route" />
+<h3>Params: {{ $route.params.id }}</h3>
 </template>
 
 <script>
 import Title from '../components/Title.vue'
-
 export default {
     components: { 
         Title 
     },
-    data() {
-        return {
-            arrayBlog: []
-        }
-    },
-    methods: {
-        async fetchApi() {
-            try {
-                const data = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-                const array = await data.json()
-                console.log(array)
-                this.arrayBlog = array;
-            } catch (error) {
-                console.log(error)
-            }
-        }
-    },
-    created() {
-        this.fetchApi()
-    }
 }
 </script>
-
-<style scoped>
-.btn {
-    width: 90px;
-    background-color: rgb(10, 69, 233);
-    color: white;
-}
-.titles {
-    display: flex;
-    align-items: flex-start;
-    margin-top: 6px;
-}
-</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,13 +1,56 @@
 <template>
-  <Title text="Blog title" />
+    <div class="articles">
+        <Title text="Articles" />
+        <hr/>
+        <br/>
+        <!-- <button class="btn" @click="fetchApi">Fetch API</button> -->
+        <div class="titles" v-for="item in arrayBlog" :key="item.id">
+            <router-link :to="`/blog/${item.id}`">
+                {{item.id}} - {{ item.title }}
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
 import Title from '../components/Title.vue'
 
 export default {
-    components: {
-        Title
+    components: { 
+        Title 
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {
+        async fetchApi() {
+            try {
+                const data = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+                const array = await data.json()
+                console.log(array)
+                this.arrayBlog = array;
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
+    created() {
+        this.fetchApi();
     }
 }
 </script>
+
+<style scoped>
+.btn {
+    width: 90px;
+    background-color: rgb(10, 69, 233);
+    color: white;
+}
+.titles {
+    display: flex;
+    align-items: flex-start;
+    margin-top: 6px;
+}
+</style>
