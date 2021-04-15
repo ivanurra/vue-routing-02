6980fc33708e26491e72ddb31cad5d73af188077
ivanurra <ivan.urra.wd@gmail.com>
2021-04-15 11:38:26 +0200

    params route

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,6 +1,8 @@
 <template>
 <Title text="Params Route" />
 <h3>Params: {{ $route.params.id }}</h3>
+<h3>{{ article.title }}</h3>
+<p>{{ article.body }}</p>
 </template>
 
 <script>
@@ -9,5 +11,25 @@ export default {
     components: { 
         Title 
     },
+    data() {
+        return {
+            article: {}
+        }
+    },
+    methods: {
+        async fetchApi() {
+            try {
+                const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+                const array = await data.json()
+                console.log(array)
+                this.article = array
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
+    created() {
+        this.fetchApi();
+    }
 }
 </script>
